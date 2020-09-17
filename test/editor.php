c86de553ac13a525d5367a3036ac5fdd639f3eee
ukhyun2402 <hyun4911@gmail.com>
2020-09-17 23:14:40 +0900

    editor test

--- test/editor.php
+++ test/editor.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="./ckeditor/ckeditor.js"></script>
     <title>Document</title>
 </head>
+
 <body>
     <!-- <div id="toolbar-container"></div> -->
     <textarea id="editor" name="editor"></textarea>
 </body>
 <script>
-    CKEDITOR.replace('editor',{
+    CKEDITOR.replace('editor', {
         filebrowserUploadUrl: "file.php",
-        extraPlugins: 'easyimage',
+        // extraPlugins: 'easyimage',
         removePlugins: 'image',
     });
 </script>
+
 </html>
