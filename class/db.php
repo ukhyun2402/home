7408e78ec2df6f6fb5912caa6e6310930253a34d
ukhyun <hyun4911@gmail.com>
2020-09-18 12:33:25 +0900

    -hotfix

--- class/db.php
+++ class/db.php
@@ -5,7 +5,7 @@
 
         public function __construct(string $db_name='ukhyun2402') {
             $this->db_name = $db_name;
-            $this->conn = new mysqli('210.114.6.140','ukhyun2402','dnr68425',$this->db_name);
+            $this->conn = new mysqli('localhost','ukhyun2402','dnr68425',$this->db_name);
             if(mysqli_errno($this->conn)){
                 printf("Connect failed: %s\n", mysqli_error($this->conn));
                 exit();
